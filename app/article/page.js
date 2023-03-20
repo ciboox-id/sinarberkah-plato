81fd196c3916ac572b3020154eb6e22fcd74e8ef
rflishrl <rafliworkspace@gmail.com>
2023-03-20 10:33:07 +0700

    style: new style article

--- app/article/page.js
+++ app/article/page.js
@@ -1,81 +1,100 @@
 import React from 'react';
 
-const articleData = [
-  {
-    imageUrl: './images/12.jpg',
-    category: 'CATEGORY',
-    title: 'Bitters hashtag waistcoat fashion axe chia unicorn',
-    description:
-      'Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.',
-    date: '12 Jun 2023',
-  },
-  {
-    imageUrl: './images/12.jpg',
-    category: 'CATEGORY',
-    title: 'Meditation bushwick direct trade taxidermy shaman',
-    description:
-      'Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.',
-    date: '12 Jun 2023',
-  },
-  {
-    imageUrl: './images/12.jpg',
-    category: 'CATEGORY',
-    title: 'Woke master cleanse drinking vinegar salvia',
-    description:
-      'Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.',
-    date: '12 Jun 2023',
-  },
-];
-
-const Article = ({ article }) => {
-  return (
-    <div className="py-8 flex flex-wrap md:flex-nowrap">
-      <div className="md:mb-0 mb-6 flex-shrink-0 flex flex-col lg:w-1/4 md:w-1/2 p-4 w-full">
-        <img
-          className="object-cover object-center w-full h-full"
-          src={article.imageUrl}
-          alt="article"
-        />
-      </div>
-      <div className="md:flex-grow ml-8 pt-8">
-        <p className="text-sm font-semibold title-font text-main-color mb-4">
-          {article.category}
-        </p>
-        <h2 className="text-2xl font-bold text-dark-main title-font mb-2">
-          {article.title}
-        </h2>
-        <p className="leading-relaxed text-gray font-semibold">
-          {article.description}
-        </p>
-        <p className="mt-6 text-gray text-sm">{article.date}</p>
-        <a className="text-dark-main inline-flex items-center mt-5 hover:text-main-color">
-          Lihat Selengkapnya
-          <svg
-            className="w-4 h-4 ml-2"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M5 12h14"></path>
-            <path d="M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-    </div>
-  );
-};
-
 const News = () => {
   return (
-    <section className="px-14">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="-my-8 divide-y-2 divide-gray-100">
-          {articleData.map((article, index) => (
-            <Article key={index} article={article} />
-          ))}
+    <section class="px-14">
+      <div class="container py-24 md:flex gap-x-8">
+        <div class="-my-8 divide-y-2 divide-gray w-2/3">
+          <div class="py-8 flex flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 mr-6 flex-shrink-0 flex flex-col">
+              <img class="w-64" src="./images/12.jpg" alt="news" />
+            </div>
+            <div class="md:flex-grow">
+              <h2 class="text-2xl font-bold text-dark-main font-mulish mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
+              <p class="leading-relaxed text-gray font-medium">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <p class="mt-5 text-gray text-sm">12 Jun 2023</p>
+              <a class="text-indigo-500 inline-flex items-center mt-4 hover:text-main-color">Lihat Selengkapnya
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="py-8 flex flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 mr-6 flex-shrink-0 flex flex-col">
+              <img class="w-64" src="./images/12.jpg" alt="news" />
+            </div>
+            <div class="md:flex-grow">
+              <h2 class="text-2xl font-bold text-dark-main font-mulish mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
+              <p class="leading-relaxed font-medium text-gray">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <p class="mt-5 text-gray text-sm">12 Jun 2023</p>
+              <a class="text-dark-main inline-flex items-center mt-6 hover:text-main-color">Lihat Selengkapnya
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="py-8 flex flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 mr-6 flex-shrink-0 flex flex-col">
+              <img class="w-64" src="./images/12.jpg" alt="news" />
+            </div>
+            <div class="md:flex-grow">
+              <h2 class="text-2xl font-bold text-dark-main font-mulish mb-2">Woke master cleanse drinking vinegar salvia</h2>
+              <p class="leading-relaxed font-medium text-gray">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <p class="mt-5 text-gray text-sm">12 Jun 2023</p>
+              <a class="text-dark-main inline-flex items-center mt-6 hover:text-main-color">Lihat Selengkapnya
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="py-8 flex flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 mr-6 flex-shrink-0 flex flex-col">
+              <img class="w-64" src="./images/12.jpg" alt="news" />
+            </div>
+            <div class="md:flex-grow">
+              <h2 class="text-2xl font-bold text-dark-main font-mulish mb-2">Woke master cleanse drinking vinegar salvia</h2>
+              <p class="leading-relaxed font-medium text-gray">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <p class="mt-5 text-gray text-sm">12 Jun 2023</p>
+              <a class="text-dark-main inline-flex items-center mt-6 hover:text-main-color">Lihat Selengkapnya
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col justify-center items-center mt-24 md:w-1/3 md:mt-0">
+          <div class="flex flex-col items-center w-56">
+            <h1 class="sm:text-3xl text-2xl font-bold font-mulish mb-4 text-main-color">New Arrival</h1>
+          </div>
+          <div class="md:w-2/3">
+            <div class="bg-gray-100 p-6 rounded-lg">
+              <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
+              <h3 class="tracking-widest text-indark-main text-xs font-medium title-font">SUBTITLE</h3>
+              <h2 class="text-lg text-dark-main font-medium title-font mb-4">Chichen Itza</h2>
+            </div>
+          </div>
+          <div class="md:w-2/3">
+            <div class="bg-gray-100 p-6 rounded-lg">
+              <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
+              <h3 class="tracking-widest text-indark-main text-xs font-medium title-font">SUBTITLE</h3>
+              <h2 class="text-lg text-dark-main font-medium title-font mb-4">Colosseum Roma</h2>
+            </div>
+          </div>
+          <div class="md:w-2/3">
+            <div class="bg-gray-100 p-6 rounded-lg">
+              <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
+              <h3 class="tracking-widest text-indark-main text-xs font-medium title-font">SUBTITLE</h3>
+              <h2 class="text-lg text-dark-main font-medium title-font mb-4">Great Pyramid of Giza</h2>
+            </div>
+          </div>
         </div>
       </div>
     </section>
